00eec5733abde50048abd6c3a4bb1f6fecd0a420
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-15 20:49:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,9 +19,9 @@ class Song
     @@count
   end
 
-  # def self.genres #return unique genres only
-  #   @@genres.uniq # .uniq returns a new array by removing duplicate values in self
-  # end
+  def self.genres #return unique genres only
+    @@genres.uniq # .uniq returns a new array by removing duplicate values in self
+  end
 
   def self.artists #return unique artists only
     @@artists.uniq
