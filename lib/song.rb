56e918b470c417b1949f461697d8b5f2732bcd08
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-15 19:29:07 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,30 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = {}
+  @@artists = {}
+
+  def initialize
+    @@count += 1
+  end
+
+  def self.count
+    @@count
+  end
+
+  def genres #return unique genres only
+    @@genres
+  end
+
+  def artists #return unique artists only
+    @@artists
+  end
+
+  def artist_count #returns a hash in which the keys are the names of each genre
+
+  end
+
+
+end
